4cbe875eb2d30961e57bd473b613a43b6bee232b
DavidMEvans <devans6915@aol.com>
2018-02-15 21:08:40 +0000

    second

--- accounts/forms.py
+++ accounts/forms.py
@@ -59,7 +59,52 @@ class UserLoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
 
 
-class RegistrationForm(UserCreationForm):
+#class RegistrationForm(UserCreationForm):
+#    password1 = forms.CharField(
+#        label='Password',
+#        widget=forms.PasswordInput
+#    )
+
+#    password2 = forms.CharField(
+#        label='Password Confirmation',
+#        widget=forms.PasswordInput
+#    )
+
+#    class Meta:
+#        model = User
+#        fields = ['email', 'password1', 'password2']
+#        exclude = ['username']
+
+#    def clean_email(self):
+#        email = self.cleaned_data.get('email')
+#        username = self.cleaned_data.get('username')
+#        if email and User.objects.filter(email=email).exclude(username=username).count():
+#            raise forms.ValidationError(u'Email addresses must be unique.')
+#        return email
+
+#    def clean_password2(self):
+#        password1 = self.cleaned_data.get('password1')
+#        password2 = self.cleaned_data.get('password2')
+
+#        if password1 and password2 and password1 != password2:
+#            message = "Passwords do not match"
+#            raise ValidationError(message)
+
+#        return password2
+
+#    def save(self, commit=True):
+#        instance = super(RegistrationForm, self).save(commit=False)
+
+#        # automatically set to email address to create a unique identifier
+#        instance.username = instance.email
+
+#        if commit:
+#            instance.save()
+
+#        return instance
+
+
+class UserRegistrationForm(UserCreationForm):
     password1 = forms.CharField(
         label='Password',
         widget=forms.PasswordInput
@@ -74,13 +119,6 @@ class RegistrationForm(UserCreationForm):
         model = User
         fields = ['email', 'password1', 'password2']
         exclude = ['username']
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        username = self.cleaned_data.get('username')
-        if email and User.objects.filter(email=email).exclude(username=username).count():
-            raise forms.ValidationError(u'Email addresses must be unique.')
-        return email
 
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
